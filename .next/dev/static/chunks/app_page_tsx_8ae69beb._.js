6b45ffe7b7c8e3f00e97fa96fecd2ca104349b41
it-iandeepak <deepakkumar84142005@gmail.com>
2025-11-14 21:59:57 +0530

    Update .gitignore and remove sensitive files from tracking

--- .next/dev/static/chunks/app_page_tsx_8ae69beb._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9cea80ba._.js",
-  "static/chunks/node_modules_692b0a19._.js"
-],
-    source: "dynamic"
-});
